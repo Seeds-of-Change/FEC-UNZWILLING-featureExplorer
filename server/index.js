f3340971daed5523925c7fe2e105ff80aa5203ab
jonfu86 <jonfu86@gmail.com>
2020-09-25 17:57:52 -0700

    updated route and query for getting a product by id

--- server/index.js
+++ server/index.js
@@ -10,6 +10,17 @@ app.listen(port, function() {
   console.log(`listening on port ${port}`);
 });
 
+app.get('/products/:id', (req, res) => {
+  var id = req.params.id;
+  db.fetch(id)
+    .then( product => {
+      if(!product) {
+        res.status(400).send(`error finding product with id: ${id}`);
+      }
+      res.status(200).send(product);
+    })
+
+});
 
 app.use(express.static(__dirname + '/../client/dist'));
 app.use(bodyParser.json());
